86f4226428004c45ff86cc364a40417ba901675a
Meraz Alvee <167668328+merazalvee@users.noreply.github.com>
2024-07-17 16:56:21 +0600

    Update music.js

--- app/assets/javascript/music.js
+++ app/assets/javascript/music.js
@@ -1,15 +1,27 @@
-  function musicPlay() {
-        var audio = document.getElementById(&quot;audioElement&quot;);
-        var playButton = document.getElementById(&quot;rahPlayButton&quot;);
-        var pauseButton = document.getElementById(&quot;rahPauseButton&quot;);
+var playButton = document.getElementById("rahPlayButton");
+    var pauseButton = document.getElementById("rahPauseButton");
+    var audio = document.getElementById("audioElement");
+    
+    var playlist = [
+        "https://www.blogseba.com/app/assets/groove-day.mp3",
+        "https://www.blogseba.com/app/assets/tumi-hakim-hoiya-hukum-koro.mp3",
+        "https://www.blogseba.com/app/assets/amar-sonar-bangla.mp3"
+    ];
 
+    function getRandomSong() {
+        var randomIndex = Math.floor(Math.random() * playlist.length);
+        return playlist[randomIndex];
+    }
+
+    function musicPlay() {
         if (audio.paused) {
+            audio.src = getRandomSong();
             audio.play();
-            playButton.classList.add(&quot;hidden&quot;);
-            pauseButton.classList.remove(&quot;hidden&quot;);
+            playButton.classList.add("hidden");
+            pauseButton.classList.remove("hidden");
         } else {
             audio.pause();
-            playButton.classList.remove(&quot;hidden&quot;);
-            pauseButton.classList.add(&quot;hidden&quot;);
+            playButton.classList.remove("hidden");
+            pauseButton.classList.add("hidden");
         }
     }
